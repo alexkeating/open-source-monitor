806aec69153e20c2860786d22973015218e75dd2
Alexander Keating <atk315315@gmail.com>
2018-08-10 09:23:30 -0400

    Initial bot

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
+import "net/http/httputil"
+import "os"
+import "bytes"
 import "time"
 import "errors"
 import "net/http"
-import "log"
 import "fmt"
 import "io/ioutil"
-
+import "io"
 import "encoding/json"
+
+import "github.com/joho/godotenv"
+import log "github.com/Sirupsen/logrus"
 
 type Repo struct {
 	Link  string
@@ -18,6 +23,11 @@ type Repo struct {
 
 func main() {
 	var repos []Repo
+	log.SetLevel(log.DebugLevel)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
 	contents, err := OpenFile("projects.json")
 	if err != nil {
 		log.Fatal(err)
@@ -39,11 +49,12 @@ func main() {
 		if lastCheck.Hours() >= 24 {
 			continue
 		}
-		fmt.Println(latestVersion)
-		PostMastodonStatus()
+		year, month, day := latestVersion.PublishedAt.Date()
+		log.WithFields(log.Fields{"TagName": latestVersion.TagName,
+			"PublishedAt": latestVersion.PublishedAt,
+			"HTMLURl":     latestVersion.HtmlUrl}).Debug("Project metadata for message:")
+		PostMastodonStatus(fmt.Sprintf("Version %v of %v was published on %v! Check out the changes at the link below:\n\n%v", latestVersion.TagName, project.Repo, fmt.Sprintf("%v %vth %v", month, day, year), latestVersion.HtmlUrl))
 	}
-	fmt.Printf("Repos: %v", repos)
-	fmt.Printf("hello, world\n")
 }
 
 func BuildProjectType(project Repo) (Project, error) {
@@ -107,8 +118,68 @@ func (g Github) GetLatestVersion() (LatestVersion, error) {
 		HtmlUrl:     bodyJson.Html_Url}, nil
 }
 
-func PostMastodonStatus() error {
+// Get mastodon access token
+// Then Post status
+// Authorize github account
+// Testing documentation
+// Clean up split things into seperate packages
+// Logging
+
+func GetMastodonAccessToken() string {
+	return os.Getenv("ACCESS_TOKEN")
+}
+
+func MastodonRequest(method, url string, body io.Reader) (*http.Response, error) {
+	// I think this unction is doing too much
+	// It should only create a request and add the appropriate header
+	// It should be up to the calling code to execute the request
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf(`Bearer %v`, os.Getenv("ACCESS_TOKEN")))
+	req.Header.Add("Content-Type", "application/json")
+	dump, err := httputil.DumpRequestOut(req, true)
+	if err != nil {
+		return nil, err
+	}
+	log.WithFields(log.Fields{
+		"request": fmt.Sprintf("%q", dump),
+	}).Debug("Request to Mastodon:")
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+// send status request if the update was in
+type MastodonStatusRequest struct {
+	Status     string `json:"status"`
+	Visibility string `json:visibility`
+}
+
+func PostMastodonStatus(status string) error {
+	request := MastodonStatusRequest{
+		Status:     status,
+		Visibility: "public",
+	}
+	data, err := json.Marshal(request)
+	if err != nil {
+		return err
+	}
+	resp, err := MastodonRequest("POST", "https://mastodon.alexkeating.me/api/v1/statuses", bytes.NewReader(data))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode > 399 {
+		return errors.New(fmt.Sprintf("Response to %v returned had a status: %v", "mastodon.alexkeating.me", resp.Status))
+	}
 	return nil
+
 }
 
 func OpenFile(path string) ([]byte, error) {
